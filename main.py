489ceec8b72b551253e21697d76eadd358e05418
WyattArnsdorf <wyatt.arnsdorf123@gmail.com>
2024-08-04 23:02:45 -0700

    Added Profile Management Microservice

--- main.py
+++ main.py
@@ -1,35 +1,178 @@
 from device_inventory import Inventory
+import time
+import zmq
+import json
 
 ####################################################Main#################################################
 def main():
-    print("\n")
-    print_ui("Version")
     while True:
-        print("\n")
-        print_ui("Home")
-        print("\n")
-        print("Please enter option: ", end=" ")
-        user_input = input().strip()
-        #Access the inventory documents
-        if user_input == "1":
-            inventory_object = Inventory()
-            inventory_object.main_inventory()
-    
-        #elif user_input == "2":
-            #section = "_Configuration Files_"
+        authentication = login_operations()
+        #If the user is verified
+        if authentication == '1': 
+            print("\n")
+            print_ui("_Version_")
             
-        #quit the main program
-        elif user_input == "Q" or user_input == "q":
+            while True:
+                print("\n")
+                print_ui("Home")
+                print("\n")
+                print("Please enter option: ", end=" ")
+                user_input = input().strip()
+                #Access the inventory documents
+                if user_input == "1":
+                    inventory_object = Inventory()
+                    inventory_object.main_inventory()
+
+                #elif user_input == "2":
+                    #section = "_Configuration Files_"
+
+                #quit the main program
+                elif user_input == "Q" or user_input == "q":
+                    authentication = '2'
+                    return authentication
+        #If the user cannot be verified
+        elif authentication == '0':
+            print("\nUnable to authenticate. Please try again: ")
+        #Quit Program
+        elif authentication == '2':
             break
 
-            
+
 #################################################Functions##############################################
 
 #***************************************************************************# 
-# Type: Helper Function 
-# Function Name: Print UI
-# Parameters: setcion_title
-# Returns: n/a
+# Type: Helper Function
+# Function Name: Login Operations
+# Description: Initiates and manages the login/sign-up process by prompting for their credentials
+# Parameters: n/a
+# Returns: returns result of login operations
+#
+#***************************************************************************#
+def login_operations():
+    print("\n")
+    print_ui("_Login_")
+    authentication = ''
+    while True:
+        print("\nIf you have an account, login. If you are not registered, signup. ")
+        print("\nPlease enter option: ", end="")
+        user_input = input()
+
+        #Login
+        if user_input == "1":
+            #get user credentials
+            print("\nPlease enter your user ID: ", end="")
+            id = input()
+            print("\nPlease enter your Password: ", end="")
+            password = input()
+            #establish the key
+            password_key = establish_key(password)
+            #use key and login ID to get response
+            response_data = send_request("Login", id, password_key)
+            if response_data == {"error": "Username not found"}:
+                authentication = '0'
+                return authentication
+            
+            else:
+                authentication = '1'
+                print(f"Received: {response_data}")
+                return authentication
+        #Sign Up
+        elif user_input == "2":
+            #get user credentials
+            print("\nPlease enter your user ID: ", end="")
+            id = input()
+            print("\nPlease enter your Password: ", end="")
+            password = input()
+            #takes the provided password and turns it into a usable key
+            password_key = establish_key(password)
+            print("\nPlease enter your email: ", end="")
+            email = input()
+            print("\nPlease enter your role (""admin"", ""user""): ", end="")
+            role = input()
+            response_data = send_request("Sign Up", id, password_key, email, "2024", role)
+
+            if response_data == {"error": "Username already exists"}:
+                print("\nUsername already exists")
+
+            else:
+                print("\nSuccessful. Please Login")
+                break
+
+        #Quit program
+        elif user_input == "Q" or user_input == "q":
+            #returns "2" from both functions to kill the servers
+            authentication  = establish_key('2')
+            null = send_request('Q')
+            return authentication 
+
+        else:
+            print("\nIncorrect option, please try again. ")
+
+#***************************************************************************# 
+# Type: Helper Function
+# Function Name: Establish Key
+# Description: connects to the convertor microservice to generate a key from the provided password
+# Parameters: password
+# Returns: hex_key
+#
+#***************************************************************************#
+def send_request(action, user_id=None, hex_key=None, email=None, year=None, role=None):
+    context = zmq.Context()
+    socket = context.socket(zmq.REQ)
+    socket.connect("tcp://localhost:6666")
+
+    request_payload = {
+        "action": action,
+        "user_id": user_id,
+        "hex_key": hex_key,
+        "email": email,
+        "year": year,
+        "role": role
+    }
+
+    socket.send_string(json.dumps(request_payload))
+    response = socket.recv_string()
+    response_data = json.loads(response)
+    context.destroy()
+    return response_data
+
+#***************************************************************************# 
+# Type: Helper Function
+# Function Name: Establish Key
+# Description: connects to the convertor microservice to generate a key from the provided password
+# Parameters: password
+# Returns: hex_key
+#
+#***************************************************************************#
+def establish_key(password):
+    #initialize binding environment
+    context = zmq.Context()
+    #establish sicket type "REQ: request"
+    socket = context.socket(zmq.REQ)
+    #bind the socket using the correct protocol and port # 5555
+    socket.connect("tcp://localhost:5555")
+
+    if password == '2':
+        #Kill server processing.
+        socket.send_string('Q')
+        return '2'
+    
+    else:
+        #send msg through the socket
+        socket.send_string(password)
+        #get the hex_key from the server
+        hex_key = socket.recv_string()
+
+        context.destroy()
+        return hex_key
+
+
+#***************************************************************************# 
+# Type: 
+# Function Name:
+# Description: 
+# Parameters:
+# Returns:
 #
 #***************************************************************************#
 def print_ui(section_title):
@@ -58,6 +201,7 @@ def print_ui(section_title):
 #***************************************************************************# 
 # Type: 
 # Function Name:
+# Description: 
 # Parameters:
 # Returns:
 #
---
+++ profile_management.py
@@ -0,0 +1,100 @@
+import time
+import zmq
+import json
+
+#Initialized dictionary for user database
+user_profile_database = {
+    "Wyatt": {
+        "hex_key": "0x480x760x310x320x330x340x350x360x37",
+        "data": ["wyatt@gmail.com", "2024", "admin"]
+    }
+}
+
+#***************************************************************************# 
+# Type: Helper Function
+# Function Name: Process User Sign Up
+# Description: processes a users sign up information to add it into the system as long
+#              as a user doesn't currently have the same user ID
+# Parameters: request_payload
+# Returns: json.dumps()
+#
+#***************************************************************************#
+def process_user_sign_up(request_payload):
+    #load the json payload into usable variables
+    request = json.loads(request_payload)
+    user_id = request.get("user_id", "default_value")
+    hex_key = request.get("hex_key", "default_value")
+    email = request.get("email", "default_value")
+    year = request.get("year", "default_value")
+    role = request.get("role", "default_value")
+
+    if user_id in user_profile_database:
+        return json.dumps({"error": "Username already exists"})
+    
+    else:
+        user_profile_database[user_id] = {
+        "hex_key": hex_key,
+        "data": [email, year, role]
+        }
+        
+        return json.dumps({"message": "Successful. Please Login"})
+
+
+#***************************************************************************# 
+# Type: Helper Function
+# Function Name: Process User Login
+# Description: processes a users login information to verify there access into the system
+# Parameters: request_payload
+# Returns: json.dumps()
+#
+#***************************************************************************#
+def process_user_login(request_payload):
+    #load the json payload into usable variables
+    request = json.loads(request_payload)
+    user_id = request.get("user_id", "default_value")
+    hex_key = request.get("hex_key", "default_value")
+
+    if user_id in user_profile_database:
+        if user_profile_database[user_id]["hex_key"] == hex_key:
+            #Return user data as JSON string
+            return json.dumps(user_profile_database[user_id]["data"])
+        else:
+            #Return error message as JSON string
+            return json.dumps({"error": "Invalid hexadecimal key"})
+    else:
+        #Return error message as JSON string
+        return json.dumps({"error": "Username not found"})
+
+
+#initiate the socket context to wait for a reply from the client
+context = zmq.Context()
+socket = context.socket(zmq.REP)
+socket.bind("tcp://*:6666")
+
+while True:
+    #Listen for request
+    print("\nListening.") 
+    load_request = socket.recv_string()
+
+    #Process the request if its an actual payload
+    if len(load_request) > 0:
+        #receive the load to extrapilate the action required to preform
+        true_request = json.loads(load_request)
+        action = true_request.get("action")
+        #If the action is to kill the server
+        if action == 'Q':
+            print("\nProcess Killed.")
+            context.destroy()
+            break
+        #if the action is to login
+        elif action == 'Login':
+            response = process_user_login(load_request)
+            #send the response back to the client
+            socket.send_string(response)
+        #if the action is to sign up
+        elif action == 'Sign Up':
+            response = process_user_sign_up(load_request)
+            #send the response back to the client
+            socket.send_string(response)
+
+    time.sleep(3)
